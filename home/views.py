645b9d07c20879245071ace198a606b3abb3f905
redcliver <igor-peres@hotmail.com>
2018-08-21 17:09:27 -0300

    1.01

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from contas.models import conta
+from django.utils import timezone
 # Create your views here.
 def home(request):
     if request.user.is_authenticated():
-        return render(request, 'home/home.html', {'title':'Home'})
+        try:
+            hoje = timezone.now().strftime("Y-m-d")
+            contagem = 0
+            for c in conta.objetas.filter(estado=1):
+                if c.data_venc <= hoje:
+                    contagem = contagem + 1
+        except:
+            contagem = 0
+        return render(request, 'home/home.html', {'title':'Home','contagem':contagem})
     else:
         return render(request, 'home/erro.html', {'title':'Erro'})
 
